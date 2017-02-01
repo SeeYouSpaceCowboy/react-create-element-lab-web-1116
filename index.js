7aedd6822f9e21a652afec9ce8e168178a70a4c7
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-01 14:31:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//Code React element here
+const javascript = React.createElement('li', {}, 'JavaScript');
+const react = React.createElement('li', {}, 'React');
+const movies = React.createElement('li', {}, 'Movies');
+const iceCream = React.createElement('li', {}, 'Ice cream');
+
+const ul = React.createElement('ul', { className: 'me__interests'}, [javascript, react, movies, iceCream]);
+const p = React.createElement('p', {}, 'Who is learning React');
+const h1 = React.createElement('h1', {}, 'An Awesome Person');
+const meInReact = React.createElement('div', { className: 'me' }, [h1, p, ul]);
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
